fa78dd042836e5b1b4cff697d9954e18cd0ae9eb
NatanMaia-ops <natanmaia2005@gmail.com>
2026-03-31 16:53:25 -0300

    create schemas and end_point routes

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,8 +1,35 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+app = FastAPI(title="Minha API BALA")
+
+database = []
 
 
-@app.get("/")
+@app.get("/", response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
-    return "Batatinhas Fritas voadoras"
+    return {"message": "Olá Mundo!"}
+
+
+@app.get("/users/", response_model=UserList, status_code=HTTPStatus.OK)
+def read_users():
+    return {"users": database}
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1,
+    )
+    # aqui precisamos criar um novo modelo que represente o banco
+    # precisamos de um identificador para esse registro
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,54 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
-client = TestClient(app)
+
+# bloco de teste de codigo reutilizavel
+@pytest.fixture
+def client():
+    return TestClient(app)
 
 
-def test_read_root():
+def test_read_root(client):
 
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == 'Batatinhas Fritas voadoras'
+    assert response.json() == {"message": "Olá Mundo!"}
+
+
+def test_create_user(client):
+    client = TestClient(app)
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
+
+
+def read_users(client):
+
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "alice",
+                "email": "alice@example.com",
+                "id": 1,
+            },
+        ]
+    }
