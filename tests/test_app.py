549f372a05a196ce4a4787152540d7897b066afd
NatanMaia-ops <natanmaia2005@gmail.com>
2026-03-31 17:53:26 -0300

    create update and delete user

--- tests/test_app.py
+++ tests/test_app.py
@@ -52,3 +52,47 @@ def read_users(client):
             },
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": 1,
+    }
+
+
+def test_update_user_404(client):
+    response = client.put(
+        "/users/999",
+        json={"username": "err", "email": "err@ex.com", "password": "123"},
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_delete_user_ok(client):
+    client.post(
+        "/users/",
+        json={"username": "zap", "email": "z@z.com", "password": "123"},
+    )
+
+    response = client.delete("/users/1")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()["username"] == "zap"
+
+
+def test_delete_user_404(client):
+    response = client.delete("/users/999")
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
